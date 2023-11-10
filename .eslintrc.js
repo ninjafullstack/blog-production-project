9b523f01256f3f22ef5c1638bb787d5fdb1c2e40
ninjafullstack <diana.sevenz.cubex@gmail.com>
2023-11-10 12:03:04 +0200

    feat: features

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:i18next/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['**/src/**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'i18next',
+  ],
+  rules: {
+    'react/jsx-indent': [2, 2],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'warn',
+    'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error',
+      {
+        markupOnly: true,
+        onlyAttribute: [''],
+        ignoreAttribute: ['data-testid', 'to'],
+      },
+    ],
+    'max-len': ['error', { ignoreComments: true, code: 100 }],
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+};
